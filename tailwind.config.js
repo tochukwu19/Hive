fd4265062c646f5240dfea2d66fc2b904bae7c23
Onwunali Tochukwu <onwunali91@yahoo.com>
2024-09-10 18:54:10 +0100

    ft: fixed discount widget error and fixed product block layout issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './layout/*.liquid',
-      './sections/*.liquid',
-      './templates/*.liquid',
-      './snippets/*.liquid',
-      './templates/customers/*.liquid',
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
 }
-
